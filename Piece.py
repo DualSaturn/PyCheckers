4d06df331016c56f18dea1c99d62084c97df4fc7
DualSaturn <saturn031000@gmail.com>
2017-01-01 16:48:49 +0530

    initial commit

---
+++ Piece.py
@@ -0,0 +1,6 @@
+class piece(object):
+    #class for each piece on the board
+    def __init__(self, row, column, color):
+        self.row = row
+        self.column = column
+        self.color = color
---
+++ main.py
@@ -0,0 +1,76 @@
+#main file for checkers game
+from Piece import piece
+import pygame
+pygame.init()
+
+#initializing values
+surfaceWidth = 750
+surfaceHeight = 750
+boardWidth = 500
+boardHeight = 500
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+clock = pygame.time.Clock()
+
+#creating board
+board = [[piece(i, j, green) for j in range(8)] for i in range(8)]
+#def resetBoard():
+#    for i in range(8):
+#        for j in range(8):
+#            board[i][j](piece(i, j, white))
+
+#creating game surface
+gameSurface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
+pygame.display.set_caption("Checkers by Varun")
+
+#function to draw all pieces
+def drawPieces():
+    xCoordinate = 156.25
+    yCoordinate = 156.25
+    for i in range(8):
+        for j in range(8):
+            pygame.draw.circle(gameSurface, board[i][j].color, (int(xCoordinate), int(yCoordinate)), 20)
+            xCoordinate = xCoordinate + 62.5
+        yCoordinate = yCoordinate + 62.5
+        xCoordinate = 156.25
+
+#function to draw the board's outline
+def drawBoardOutline():
+    lines = [
+    ((125, 625), (625, 625)), #bottom
+    ((125, 125), (625, 125)), #top
+    ((625, 125), (625, 625)), #right
+    ((125, 125), (125, 625)), #left
+    ((125 + boardWidth/2, 125), (125 + boardWidth/2, 625)), #vertical 1/2
+    ((125, 125 + boardHeight/2), (625, 125 + boardHeight/2)), #horizontal 1/2
+    ((125 + boardWidth/4, 125), (125 + boardWidth/4, 625)), #vertical 1/4
+    ((125, 125 + boardHeight/4), (625, 125 + boardHeight/4)), #horizontal 1/4
+    ((125 + 3*boardWidth/4, 125), (125 + 3*boardWidth/4, 625)), #vertical 3/4
+    ((125, 125 + 3*boardHeight/4), (625, 125 + 3*boardHeight/4)), #horizontal 3/4
+    ((125 + boardWidth/8, 125), (125 + boardWidth/8, 625)), #vertical 1/8
+    ((125, 125 + boardHeight/8), (625, 125 + boardHeight/8)), #horizontal 1/8
+    ((125 + 3*boardWidth/8, 125), (125 + 3*boardWidth/8, 625)), #vertical 3/8
+    ((125, 125 + 3*boardHeight/8), (625, 125 + 3*boardHeight/8)), #horizontal 3/8
+    ((125 + 5*boardWidth/8, 125), (125 + 5*boardWidth/8, 625)), #vertical 5/8
+    ((125, 125 + 5*boardHeight/8), (625, 125 + 5*boardHeight/8)), #horizontal 5/8
+    ((125 + 7*boardWidth/8, 125), (125 + 7*boardWidth/8, 625)), #vertical 7/8
+    ((125, 125 + 7*boardHeight/8), (625, 125 + 7*boardHeight/8)) #horizontal 7/8
+    ]
+    for x in lines:
+        pygame.draw.line(gameSurface, white, x[0], x[1], 3)
+
+#main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+    gameSurface.fill(black)
+    drawBoardOutline()
+    drawPieces()
+    pygame.display.update()
+    clock.tick(60)
