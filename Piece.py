653b86ffd05f4f4b9ecde23f7fa0cf4427a9a713
DualSaturn <saturn031000@gmail.com>
2017-01-03 14:04:14 +0530

    changed piece drawing implementation
    I moved the pygame.draw.circle() call to the piece class.

--- Piece.py
+++ Piece.py
@@ -1,13 +1,17 @@
+import pygame
 class piece(object):
     #class for each piece on the board
-    def __init__(self, row, column):
+    def __init__(self, row, column, x, y, surf):
         self.row = row
         self.column = column
         self.color = self.startColor()
-        
+        self.pos_x = int(x)
+        self.pos_y = int(y)
+        self.surf = surf
+
     def startColor(self):
         green = (0, 255, 0)
-        blue = (0, 0, 255)
+        blue = (0, 100, 255)
         black = (0, 0, 0)
         col = black
         if self.row % 2 == 0:
@@ -34,3 +38,6 @@ class piece(object):
                     col = black
 
         return col
+
+    def draw(self):
+        pygame.draw.circle(self.surf, self.color, (self.pos_x, self.pos_y), 20)
