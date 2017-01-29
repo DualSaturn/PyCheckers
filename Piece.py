9e80245a3292a03c584adf43666d0cfab2fbae32
DualSaturn <saturn031000@gmail.com>
2017-01-29 12:28:16 +0530

    refactored code
    code was refactored based on PEP 8 guildelines

--- Piece.py
+++ Piece.py
@@ -6,17 +6,18 @@ blue = (0, 100, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
-class piece(object):
-    #class for each piece on the board
+
+class Piece(object):
+    # class for each piece on the board
     def __init__(self, row, column, x, y, surf):
         self.row = row
         self.column = column
-        self.color = self.startColor()
+        self.color = self.start_color()
         self.pos_x = int(x)
         self.pos_y = int(y)
         self.surf = surf
 
-    def startColor(self):
+    def start_color(self):
         col = black
         if self.row % 2 == 0:
             if self.row <= 2:
@@ -48,12 +49,12 @@ class piece(object):
 
     def clicked(self, click_x, click_y):
         dist = math.hypot(click_x - self.pos_x, click_y - self.pos_y)
-        if dist>20:
+        if dist > 20:
             return False
         else:
             return True
 
-    def handleClick(self, click_x, click_y):
+    def handle_click(self, click_x, click_y):
         if self.clicked(click_x, click_y):
             if self.color != black:
                 self.color = white
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#main file for checkers game
-from Piece import piece
+# main file for checkers game
+from Piece import Piece
 import pygame
 pygame.init()
 
-#initializing values
+# initializing values
 surfaceWidth = 750
 surfaceHeight = 750
 boardWidth = 500
@@ -15,63 +15,67 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 clock = pygame.time.Clock()
 
-#creating game surface
+# creating game surface
 gameSurface = pygame.display.set_mode((surfaceWidth, surfaceHeight))
 pygame.display.set_caption("PyCheckers")
 
-#creating board
-#board = [[piece(i, j) for j in range(8)] for i in range(8)]
+# creating board
+# board = [[piece(i, j) for j in range(8)] for i in range(8)]
 board = []
-def createBoard():
-    xCoordinate = 156.25
-    yCoordinate = 156.25
+
+
+def create_board():
+    x_coordinate = 156.25
+    y_coordinate = 156.25
     for i in range(8):
         board.append([])
         for j in range(8):
-            board[i].append(piece(i, j, xCoordinate, yCoordinate, gameSurface))
-            xCoordinate = xCoordinate + 62.5
-        yCoordinate = yCoordinate + 62.5
-        xCoordinate = 156.25
+            board[i].append(Piece(i, j, x_coordinate, y_coordinate, gameSurface))
+            x_coordinate += 62.5
+        y_coordinate += 62.5
+        x_coordinate = 156.25
 
 
-def resetBoard():
+def reset_board():
     for i in range(8):
         for j in range(8):
-            board[i][j].color = board[i][j].startColor()
+            board[i][j].color = board[i][j].start_color()
 
-#function to draw all pieces
-def drawPieces():
+
+# function to draw all pieces
+def draw_pieces():
     for i in range(len(board)):
         for j in range(len(board[i])):
             board[i][j].draw()
 
-#function to draw the board's outline
-def drawBoardOutline():
+
+# function to draw the board's outline
+def draw_board_outline():
     lines = [
-    ((125, 625), (625, 625)), #bottom
-    ((125, 125), (625, 125)), #top
-    ((625, 125), (625, 625)), #right
-    ((125, 125), (125, 625)), #left
-    ((125 + boardWidth/2, 125), (125 + boardWidth/2, 625)), #vertical 1/2
-    ((125, 125 + boardHeight/2), (625, 125 + boardHeight/2)), #horizontal 1/2
-    ((125 + boardWidth/4, 125), (125 + boardWidth/4, 625)), #vertical 1/4
-    ((125, 125 + boardHeight/4), (625, 125 + boardHeight/4)), #horizontal 1/4
-    ((125 + 3*boardWidth/4, 125), (125 + 3*boardWidth/4, 625)), #vertical 3/4
-    ((125, 125 + 3*boardHeight/4), (625, 125 + 3*boardHeight/4)), #horizontal 3/4
-    ((125 + boardWidth/8, 125), (125 + boardWidth/8, 625)), #vertical 1/8
-    ((125, 125 + boardHeight/8), (625, 125 + boardHeight/8)), #horizontal 1/8
-    ((125 + 3*boardWidth/8, 125), (125 + 3*boardWidth/8, 625)), #vertical 3/8
-    ((125, 125 + 3*boardHeight/8), (625, 125 + 3*boardHeight/8)), #horizontal 3/8
-    ((125 + 5*boardWidth/8, 125), (125 + 5*boardWidth/8, 625)), #vertical 5/8
-    ((125, 125 + 5*boardHeight/8), (625, 125 + 5*boardHeight/8)), #horizontal 5/8
-    ((125 + 7*boardWidth/8, 125), (125 + 7*boardWidth/8, 625)), #vertical 7/8
-    ((125, 125 + 7*boardHeight/8), (625, 125 + 7*boardHeight/8)) #horizontal 7/8
+     ((125, 625), (625, 625)),  # bottom
+     ((125, 125), (625, 125)),  # top
+     ((625, 125), (625, 625)),  # right
+     ((125, 125), (125, 625)),  # left
+     ((125 + boardWidth/2, 125), (125 + boardWidth/2, 625)),  # vertical 1/2
+     ((125, 125 + boardHeight/2), (625, 125 + boardHeight/2)),  # horizontal 1/2
+     ((125 + boardWidth/4, 125), (125 + boardWidth/4, 625)),  # vertical 1/4
+     ((125, 125 + boardHeight/4), (625, 125 + boardHeight/4)),  # horizontal 1/4
+     ((125 + 3*boardWidth/4, 125), (125 + 3*boardWidth/4, 625)),  # vertical 3/4
+     ((125, 125 + 3*boardHeight/4), (625, 125 + 3*boardHeight/4)),  # horizontal 3/4
+     ((125 + boardWidth/8, 125), (125 + boardWidth/8, 625)),  # vertical 1/8
+     ((125, 125 + boardHeight/8), (625, 125 + boardHeight/8)),  # horizontal 1/8
+     ((125 + 3*boardWidth/8, 125), (125 + 3*boardWidth/8, 625)),  # vertical 3/8
+     ((125, 125 + 3*boardHeight/8), (625, 125 + 3*boardHeight/8)),  # horizontal 3/8
+     ((125 + 5*boardWidth/8, 125), (125 + 5*boardWidth/8, 625)),  # vertical 5/8
+     ((125, 125 + 5*boardHeight/8), (625, 125 + 5*boardHeight/8)),  # horizontal 5/8
+     ((125 + 7*boardWidth/8, 125), (125 + 7*boardWidth/8, 625)),  # vertical 7/8
+     ((125, 125 + 7*boardHeight/8), (625, 125 + 7*boardHeight/8))  # horizontal 7/8
     ]
     for x in lines:
         pygame.draw.line(gameSurface, white, x[0], x[1], 3)
 
-#main game loop
-createBoard()
+# main game loop
+create_board()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -81,10 +85,10 @@ while True:
             mouse_pos = pygame.mouse.get_pos()
             for i in range(len(board)):
                 for j in range(len(board[i])):
-                    board [i][j].handleClick(mouse_pos[0], mouse_pos[1])
+                    board[i][j].handle_click(mouse_pos[0], mouse_pos[1])
             
     gameSurface.fill(black)
-    drawBoardOutline()
-    drawPieces()
+    draw_board_outline()
+    draw_pieces()
     pygame.display.update()
     clock.tick(60)
